e0e84ee6d109c77bf55e04b1fdd94c2d32edd584
Mauro <mj.silva7@gmail.com>
2023-07-10 16:48:20 -0300

    Metodos editar e excluir com modal de confirmação

--- src/main/java/com/kamauro/mvcudemy/controller/DepartamentoController.java
+++ src/main/java/com/kamauro/mvcudemy/controller/DepartamentoController.java
@@ -2,7 +2,9 @@ package com.kamauro.mvcudemy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,7 +24,8 @@ public class DepartamentoController {
     }
 
     @GetMapping("/listar")
-    public String listar() {
+    public String listar(ModelMap model) {
+        model.addAttribute("departamentos", service.buscarTodos());
         return "/departamento/lista";
     }
 
@@ -30,6 +33,26 @@ public class DepartamentoController {
     public String salvar(Departamento departamento) {
         service.salvar(departamento);
         return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("editar/{id}")
+    public String abrir(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("departamento", service.buscarPorId(id));
+        return "/departamento/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Departamento departamento) {
+        service.editar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id")Long id, ModelMap model) {
+       if(service.departamentoTemCargos(id)) {
+        service.excluir(id);
+       }
+        return listar(model);
     }
     
 }
